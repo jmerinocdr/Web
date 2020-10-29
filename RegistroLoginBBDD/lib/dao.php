7e1caaed3f676abd92b8595d10ca1c02fff80d2d
jaime <jmerino.cdr@gmail.com>
2020-10-29 10:06:31 +0100

    Continuación registro login

--- RegistroLoginBBDD/lib/dao.php
+++ RegistroLoginBBDD/lib/dao.php
@@ -1,27 +1,40 @@
 <?php 
 	class DAO
 	{
+		//Establecemos la variable de la conexión
+		private $conn;
+
+		//Creamos el constructor del DAO
 		function __construct($service, $user, $pass, $host, $dbname){
-			if($this->bbExist()){
 				$this->conectar($service, $user, $pass, $host);
-			}
 		}
 
-		//Getters&Setters
+		//Getters y Setters para obtener la conexión
 		public function setDB($conn){
 			$this->conn=$conn;
 		}
 		public function getDB(){
-			return $this->conn;
+			return $this->$conn;
 		}
 
+		//Incluimos un metodo para comprobar si la base de datos está creada
+		public function dbExist($conn){
+			if($this.getDB() != null){
 
+			}
+		}
 
 		//Usamos la Conexión y Ejecución de la base de datos
 		public function conectar($service, $user, $pass, $host, $dbname){
 			$dbInfo = $service . ":host=" . $host . ";dbname=" . $dbname;
-			$conn = new PDO($dbInfo, $user, $pass);
-			return $conn;
+			try{
+				$conn = new PDO($dbInfo, $user, $pass)
+				$this->setDB($conn);
+				return true;
+			}
+			catch(PDOException $e){
+				return false;
+			}
 		}
 		public function ejecutar($conn, $sql, $array){
 			$stmt=$conn->prepare($sql);
@@ -110,6 +123,64 @@
 					";
 				break;
 			}
+		}
+
+		// Incluimos los metodos para crear la base de datos
+		public function crearTablas($conn){
+			$this->crearTablaUsuario($conn);
+			$this->crearTablaDeporte($conn);
+			$this->crearTablaUsuarioDeporte($conn);
+			$this->crearTablaPassw($conn);
+		}
+		private function crearBaseDatos($conn){
+			$sql = "
+				CREATE DATABASE Usuario;
+			";
+
+		}
+		private function crearTablaUsuario($conn){
+			$sql = "
+				CREATE TABLE usuario(
+					id INT(10) NOT NULL AUTO_INCREMENT,
+					nombre VARCHAR(15) NOT NULL,
+					nacido DATE,
+					sexo VARCHAR(2) NOT NULL CHECK(sexo IN ('H', 'M')),
+					foto VARCHAR(30), PRIMARY KEY (id)
+				);
+			";
+			ejecutar($conn, $sql, $array);
+		}
+
+		private function crearTablaUsuarioDeporte($conn){
+			$sql = "
+				CREATE TABLE usuario_deporte(
+					FOREIGN KEY (id_usuario) REFERENCES usuario(id), 
+					FOREIGN KEY (id_deportes) REFERENCES deporte(id)
+				);
+			";
+			ejecutar($conn, $sql, $array);
+		}
+
+		private function crearTablaDeporte($conn){
+			$sql = "
+				CREATE TABLE deporte(
+					id INT(10) NOT NULL AUTO_INCREMENT,
+					nombre VARCHAR(20) NOT NULL,
+					PRIMARY KEY (id)
+				);
+			";
+			ejecutar($conn, $sql, $array);
+		}
+
+		private function crearTablaPassw($conn){
+			$sql = "
+				Create table passwd(
+					usuario VARCHAR(15) NOT NULL,
+					clave VARCHAR(15) NOT NULL,
+					PRIMARY KEY (usuario) 
+				);
+			";
+			ejecutar($conn, $sql, $array);
 		}
 		
 	}
--- RegistroLoginBBDD/lib/login.lib.php
+++ RegistroLoginBBDD/lib/login.lib.php
@@ -1,11 +1,15 @@
 <?php
-	if($db->bbExist){
+    require_once('lib.php');
+    $db=new DAO($service, $user, $pass, $host, $dbname);
+    echo "Se ha creado el nuevo dao con los datos".$service.$user.$pass.$host,$dbname;
+    $db->conectar();
+	if($db->dbExist()){
         $user=$_POST["user"];
         $password=$_POST["password"];
-        if($db->comprobarPswrd($user, $password){
+        if($db->comprobarPswrd($user, $password)){
             session_start();
             // Guardar datos de sesión
-            $_SESSION["usuario"] = $_POST["email"];
+            $_SESSION["usuario"] = $_POST["user"];
             $_SESSION["autorizado"] = "true";
             session_regenerate_id();
             echo "Accedido correctamente";
@@ -18,5 +22,6 @@
     }
     else{
     	echo "La base de datos no existe";
+        $db->crearBase();
         header('Location: ../Index.php');
     }
